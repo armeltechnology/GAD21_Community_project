e34fc23ff31ff72a0401c1ac6c55a79753061bd2
armeltechnology <95145604+armeltechnology@users.noreply.github.com>
2022-01-03 21:56:22 +0100

    Update initial_configuration_ngrh.sh

--- initial_configuration_ngrh.sh
+++ initial_configuration_ngrh.sh
@@ -11,6 +11,25 @@ export GCLOUD_BUCKET=$DEVSHELL_PROJECT_ID-ngrh
 
 echo "Creating Cloud network and subnetwork"
 gcloud compute networks create ngrhnet --subnet-mode=custom
-gcloud compute networks subnets create ngrhsubnet-us --network=ngrhnet --region=us-central1 --range=172.16.0.0/24
+gcloud compute networks subnets create ngrhsubnet-us --network=ngrhnet --region=us-central1 --range=10.65.61.0/24
+
+echo "Creating Cloud SQL instances"
+gcloud sql instances create [instance_Name] --tier=db-n1-standard-1 --region=us-central1
+
+echo "Set the password for the root  MySQL user "
+gcloud sql users set-password root --host=% --instance [instance_Name] --password [PASSWORD]
+
+echo "Create database"
+gcloud sql databases create [database_Name] --instance=[instance_Name]
+
+echo "create secret for data source configuration"
+echo -n "[project-Id]:us-central1:[instance_Name]" | gcloud secrets create spring_cloud_gcp_sql_instance_connection_name — replication-policy="automatic" — data-file=-  
+echo -n "[database_Name]" | gcloud secrets create spring_cloud_gcp_sql_database_name — replication-policy="automatic" — data-file=-  
+echo -n "root" | gcloud secrets create spring_datasource_username — replication-policy="automatic" — data-file=-  
+echo -n "[PASSWORD]" | gcloud secrets create spring_datasource_password — replication-policy="automatic" — data-file=-
+
+echo "list secret"
+gcloud secrets list
+
 
 echo "Project ID: $DEVSHELL_PROJECT_ID"
